bd530ff54731efd8b471150c03c3d9c4668c8e35
roshiiiz <roshaanzafar12@gmail.com>
2024-08-02 01:39:20 +0500

    Video Creation App

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+   <SafeAreaView className="bg-primary h-full">
+
+   </SafeAreaView>
+  );
+}
+
